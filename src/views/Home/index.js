a48728988260d38407e2f5de31c3c5acf1a5d6f4
douglasEvertonp <evertondouglaspereira54101@gmail.com>
2020-08-04 22:13:44 -0300

    Home

---
+++ src/views/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Home(){
+    return(
+        <ContainerHome>
+            <CotentHome>
+                <h1>hello Word</h1>
+            </CotentHome>
+        </ContainerHome>
+    )
+}
+
+export default Home

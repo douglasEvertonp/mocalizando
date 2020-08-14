c187d97967e45fc009c52de9038e9555f181585e
douglasEvertonp <evertondouglaspereira54101@gmail.com>
2020-08-14 01:29:00 -0300

    Home style

---
+++ src/views/Home/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+
+export const ContainerHome = styled.div`
+    width: 100%;
+`
+
+export const ContentHome = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`

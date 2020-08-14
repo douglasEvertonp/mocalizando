0bfcd9e25c1e23c8382ffb848139d1017c7d3cd8
douglasEvertonp <evertondouglaspereira54101@gmail.com>
2020-08-14 01:28:21 -0300

    Dados ficticios

---
+++ src/services/data.js
@@ -0,0 +1,42 @@
+
+
+
+export default {
+    categories: [
+      {
+       id: 1,
+       name: 'Pizzaria',
+       image: '/images/Pizzaria.png'
+     },
+
+     {
+        id: 2,
+        name: 'Restaurantes',
+        image: '/images/Restaurant.png'
+      },
+
+      {
+        id: 3,
+        name: 'Lanches',
+        image: '/images/Lanche.png'
+      },
+
+      {
+        id: 4,
+        name: 'Bares',
+        image: '/images/Bares.png'
+      },
+
+      {
+        id: 5,
+        name: 'Pet Shop',
+        image: '/images/Pet.png'
+      },
+
+      {
+        id: 6,
+        name: 'Estetica e Beleza',
+        image: '/images/Beleza.png'
+      },
+    ]
+}

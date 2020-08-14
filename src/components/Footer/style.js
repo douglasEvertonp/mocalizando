db4dc173f02816cb8ba357540c479658c1b21c60
douglasEvertonp <evertondouglaspereira54101@gmail.com>
2020-08-14 01:27:15 -0300

    Footer style

---
+++ src/components/Footer/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+
+export const ContainerFooter = styled.div`
+    width: 100%;
+`
+
+export const ContentFooter = styled.div`
+    width: 100%;
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #404040;
+    margin-top: 50px;
+
+        strong{
+            color: #fff;
+        }
+`

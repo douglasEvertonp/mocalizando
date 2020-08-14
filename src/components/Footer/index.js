7cc93809441fe5ca025027a790680c4812bff573
douglasEvertonp <evertondouglaspereira54101@gmail.com>
2020-08-14 01:26:43 -0300

    Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ContainerFooter, ContentFooter} from './style'
+
+function Footer(){
+    return(
+        <ContainerFooter>
+            <ContentFooter>
+                <strong>Mocalizando-2020</strong>
+            </ContentFooter>
+        </ContainerFooter>
+    )
+}
+
+export default Footer;
